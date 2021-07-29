6c1eef1fb392f74ff2197792ccb760d2f2ccfcfd
hitoshi555 <hitoshi1845@gmail.com>
2021-07-29 15:16:49 +0900

    create main function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import { Stage, Layer, Line, Image } from "react-konva";
+import { SketchPicker } from "react-color";
+import { Box, Button } from "@material-ui/core";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:9999", {
+  transports: ["websocket"],
+});
+
+type Line = {
+  tool: any;
+  points: any;
+  color: any;
+  size: any;
+};
 
 function App() {
+  const [tool, setTool] = React.useState("pen");
+  const [size, setSize] = React.useState(5);
+  const [color, setColor] = React.useState("#000000");
+  const [lines, setLines] = React.useState<Line[]>([]);
+  const [allLines, setallLines] = React.useState<Line[]>([]);
+
+  const isDrawing = React.useRef(false);
+  const stageRef = React.useRef<any>();
+
+  const emitData = (last: Line) => {
+    socket.emit("input_event", last);
+  };
+
+  socket.off("broadcast_event");
+  socket.on("broadcast_event", function (msg) {
+    console.log(msg);
+    setallLines([...allLines, msg]);
+  });
+
+  const handleMouseDown = (e: any) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setallLines([
+      ...allLines,
+      {
+        tool,
+        points: [pos.x, pos.y],
+        color,
+        size,
+      },
+    ]);
+    const last: Line = allLines.slice(-1)[0];
+    if (last !== null) {
+      emitData(last);
+    }
+    console.log(allLines);
+  };
+
+  const handleMouseMove = (e: any) => {
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = allLines[allLines.length - 1];
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+    allLines.splice(allLines.length - 1, 1, lastLine);
+    setallLines(allLines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
+  const handleChangeComplete = (color: any) => {
+    setColor(color.hex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box display="flex">
+        <select
+          value={tool}
+          onChange={(e) => {
+            setTool(e.target.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="pen">ペン</option>
+          <option value="eraser">消しゴム</option>
+        </select>
+        <select
+          value={size}
+          onChange={(e: any) => {
+            setSize(e.target.value);
+          }}
+        >
+          <option value="3">3</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+      </Box>
+      <Box display="flex">
+        <Box bgcolor="#ffffff" width="303px" height="303px">
+          <Stage
+            width={300}
+            height={300}
+            onTouchstart={handleMouseDown}
+            onTouchmove={handleMouseMove}
+            onTouchend={handleMouseUp}
+            onMouseDown={handleMouseDown}
+            onMousemove={handleMouseMove}
+            onMouseup={handleMouseUp}
+            style={{
+              border: "solid",
+              marginTop: "10px",
+              touchAction: "none",
+            }}
+            ref={stageRef}
+          >
+            <Layer>
+              {allLines.map((line, i) => (
+                <Line
+                  key={i}
+                  points={line.points}
+                  stroke={line.color}
+                  strokeWidth={line.size}
+                  tension={0.5}
+                  lineCap="round"
+                  globalCompositeOperation={
+                    line.tool === "eraser" ? "destination-out" : "source-over"
+                  }
+                />
+              ))}
+            </Layer>
+          </Stage>
+        </Box>
+        <SketchPicker color={color} onChangeComplete={handleChangeComplete} />
+      </Box>
+      <Box mt="50px">{/* <Chat /> */}</Box>
+    </>
   );
 }
 
